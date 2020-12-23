86647ee9241d5b63d06031cb041a68684aa52a44
Didy KUPANHY <d.kupanhy@gmail.com>
2020-12-23 23:57:34 +0900

    update:
    * fix new command
    * add entry and article command

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,16 @@
-use structopt::StructOpt;
-use std::io::{BufWriter, Write};
+use chrono::{Datelike, Utc};
+use serde::{Deserialize, Serialize};
+use std::env;
 use std::fs::{create_dir_all, OpenOptions};
-use chrono::{Utc, Datelike};
+use std::io::{BufReader, BufWriter, Read, Write};
+use std::path::Path;
+use structopt::StructOpt;
 
 #[derive(StructOpt, Debug)]
 #[structopt(name = "diary")]
 struct Opt {
     #[structopt(subcommand)]
-    sub: Sub
+    sub: Sub,
 }
 
 #[derive(StructOpt, Debug)]
@@ -16,86 +19,139 @@ enum Sub {
     New,
 
     #[structopt(name = "add", about = "Add a new page")]
-    Add,
+    Add(PageKind),
 }
 
+#[derive(StructOpt, Debug)]
+enum PageKind {
+    #[structopt(name = "entry", about = "Add today's entry")]
+    Entry,
+
+    #[structopt(name = "article", about = "Add new article")]
+    Article,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct DiarySetting {
+    entry_path: String,
+    article_path: String,
+    diary_template: String,
+}
 
 fn main() {
     let opt = Opt::from_args();
     let today = Utc::today();
+    let setting_file_name = "diary-cli.yaml";
 
     match &opt.sub {
-        Sub::Add => {
-            let dir_path = &format!("{}/{}", today.year(), today.month());
-            if let Err(error) = create_dir_all(dir_path) {
-                eprintln!("create_dir_all: {}", error);
+        // ページを追加
+        Sub::Add(add) => {
+            // 設定ファイルを読み込む
+            let file = OpenOptions::new().read(true).open(setting_file_name);
+            if let Err(err) = file {
+                eprintln!("diary-cli.yaml: {}", err);
                 return;
             }
+            let file = file.unwrap();
+            let mut f = BufReader::new(file);
+            let mut content = String::new();
+            f.read_to_string(&mut content).unwrap();
+            let setting: DiarySetting = serde_yaml::from_str(&content).unwrap();
 
-            let file_path = format!("{}/{}_{}_{}.md", dir_path, today.year(), today.month(), today.day());
-            let file = OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open(file_path)
-                .unwrap();
+            match add {
+                // 本日の日記
+                PageKind::Entry => {
+                    let dir_path =
+                        &format!("{}/{}/{}", setting.entry_path, today.year(), today.month());
+                    if !Path::new(dir_path).exists() {
+                        if let Err(error) = create_dir_all(dir_path) {
+                            eprintln!("create_dir_all: {}", error);
+                            return;
+                        }
+                    }
 
-            let mut f = BufWriter::new(file);
-            let content = format!(
-r#"# {}
-
-## タグ
-
-## やりたいこと
-
-## できたこと
-
-## メモ
-"#
-                                  , today.format("%Y/%m/%d"));
-            if let Err(error) = f.write(content.as_bytes()) {
-                eprintln!("f.write(content): {}", error);
+                    let file_name =
+                        format!("{}_{}_{}.md", today.year(), today.month(), today.day());
+                    let content = setting
+                        .diary_template
+                        .replace("{}", &today.format("%Y/%m/%d").to_string());
+                    write_file(dir_path, &file_name, &content);
+                }
+                // まとめなどの記事
+                PageKind::Article => {}
             }
-        },
+        }
+        // 日記を作成する
         Sub::New => {
-            if let Err(error) = create_dir_all("diary") {
-                eprintln!("create_dir_all: {}", error);
+            let directory_path = "diary";
+
+            // 日記ディレクトリまたは設定ファイルが存在してる場合はディレクトリを作成しない
+            if Path::new(directory_path).exists() || Path::new(setting_file_name).exists() {
+                eprintln!("not ok: diary or diary-cli.yaml exists");
                 return;
             }
 
-            let file = OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open("diary/README.md")
-                .unwrap();
-
-            let mut f = BufWriter::new(file);
-            if let Err(error) = f.write("# README \n".as_bytes()) {
-                eprintln!("f.write(content): {}", error);
+            if let Err(error) = create_dir_all(directory_path) {
+                eprintln!("create_dir_all: {}", error);
+                return;
+            } else {
+                println!("ok: diary directory");
             }
 
-            let file = OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open("diary/TODO.md")
-                .unwrap();
+            // 各記事へのリンク(README)
+            write_file(directory_path, "README.md", "# README \n");
 
-            let mut f = BufWriter::new(file);
-            if let Err(error) = f.write("# TODO (やるべきこと)\n".as_bytes()) {
-                eprintln!("f.write(content): {}", error);
-            }
+            // 今後のやるべきことリスト(TODO)
+            write_file(directory_path, "TODO.md", "# TODO (やるべきこと)\n");
 
-            let file = OpenOptions::new()
-                .write(true)
-                .create_new(true)
-                .open("diary/CHANGELOG.md")
-                .unwrap();
+            // 実績(CHANGELOG)
+            write_file(directory_path, "CHANGELOG.md", "# CHANGELOG (実績)\n");
 
-            let mut f = BufWriter::new(file);
-            if let Err(error) = f.write("# CHANGELOG (実績)\n".as_bytes()) {
-                eprintln!("f.write(content): {}", error);
-            }
+            // 設定ファイル
+            let diary_template = r#"# {}
 
-        },
+## Concrete Experience (具体的経験)
+
+## Reflective Observation (省察)
+
+## Abstract Conceptualization (概念化):
+
+## Active Experimentation (試行):
+
+"#;
+            let setting = DiarySetting {
+                entry_path: format!(
+                    "{}/{}/entries",
+                    env::current_dir().unwrap().display(),
+                    directory_path
+                ),
+                article_path: format!(
+                    "{}/{}/articles",
+                    env::current_dir().unwrap().display(),
+                    directory_path
+                ),
+                diary_template: diary_template.to_string(),
+            };
+            write_file(
+                directory_path,
+                setting_file_name,
+                &serde_yaml::to_string(&setting).unwrap(),
+            );
+        }
     };
+}
 
+fn write_file(directory_path: &str, file_name: &str, content: &str) {
+    let file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(format!("{}/{}", directory_path, file_name))
+        .unwrap();
+
+    let mut f = BufWriter::new(file);
+    if let Err(error) = f.write(content.as_bytes()) {
+        eprintln!("f.write(content): {}", error);
+    } else {
+        println!("ok: {}/{} file", directory_path, file_name);
+    }
 }
